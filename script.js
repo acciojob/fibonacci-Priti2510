4f1f167e85624c2c35912e354285438ad9a7a9de
Priti2510 <113327853+Priti2510@users.noreply.github.com>
2024-02-08 22:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
 // your code here
+	if (num <= 0) return "Invalid input";
+    if (num === 1) return 0;
+    if (num === 2) return 1;
+
+    let fibPrev = 0;
+    let fibCurr = 1;
+
+    for (let i = 3; i <= num; i++) {
+        let fibNext = fibPrev + fibCurr;
+        fibPrev = fibCurr;
+        fibCurr = fibNext;
+    }
+
+    return fibCurr;
 }
 
 module.exports = fibonacci;
